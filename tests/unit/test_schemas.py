40ba1d83ecb1f46dd76d7966a56595cba8114f60
Aiden Jee <aidenjee@Aidens-Air.lan>
2026-01-04 23:06:35 -0800

    Added CI pipeline and test_schemas.py

---
+++ tests/unit/test_schemas.py
@@ -0,0 +1,39 @@
+import pytest
+
+from app.schemas import *
+
+# -------------------------------------------------------------------
+# CONSTANTS (Change as needed)
+# -------------------------------------------------------------------
+UPDATE_MODEL = [
+    WorkoutUpdate, SetUpdate, ExerciseUpdate, UserUpdate, 
+    WorkoutTemplateUpdate, ExerciseTemplateUpdate
+]
+
+VALID_UPDATE_MODEL_TO_FIELD = {
+    WorkoutUpdate: {"name": "Test Run"},
+    SetUpdate: {"weight": 100.5},
+    ExerciseUpdate: {"name": "Bench Press"},
+    UserUpdate: {"name": "New Name"},
+    WorkoutTemplateUpdate: {"name": "New template"},
+    ExerciseTemplateUpdate: {"name": "New template"}
+}
+
+@pytest.mark.parametrize(
+
+)
+def test_update_error_no_field(model):
+    empty_field = {}
+    with pytest.raises(ValueError) as excinfo:
+        model(**empty_field)
+    assert "At least one value must be updated" in str(excinfo.value)
+
+def test_update_pass_one_field(model, field):
+    try:
+        instance = model(**field)
+    except Exception as e:
+        pytest.fail(f"Test failed for {model}: {e}")
+    
+    assert instance is not None, "Class not created"
+    assert len(instance.model_dump(exclude_none=True)) == 1, "Model's field != 1"
+    
