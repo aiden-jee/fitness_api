480081d2319a83dfd32b8f0879a3e4cd62fb5658
Aiden Jee <mjee@ucsd.edu>
2026-01-13 20:40:14 -0800

    reformatted for flake check

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from pydantic import Field, BaseModel, model_validator
-from datetime import datetime, timezone
+from datetime import datetime
 from typing import List, Optional, Literal
 
 # ============================================================================
